7d1e94bfe81e5c87f7477bab8452ef81b38bcd15
Cosmic Entity <monsterxgamerz12@gmail.com>
2024-09-01 23:33:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.getElementById("myButton").addEventListener("click", function() {
-	alert(" Hello, Myself Rishav Raj , Welcome to NITRR !");
+// Add event listener to nav links
+document.querySelectorAll('.nav-link').forEach(link => {
+	link.addEventListener('click', () => {
+		// Add active class to clicked link
+		link.classList.add('active');
+		// Remove active class from other links
+		document.querySelectorAll('.nav-link').forEach(otherLink => {
+			if (otherLink !== link) {
+				otherLink.classList.remove('active');
+			}
+		});
+	});
 });
+ 
